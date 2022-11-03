db6968f79c4f215777ce539c94fb3167045bc218
Kavan <kavan155gondalia@gmail.com>
2022-11-03 22:36:38 +0530

    update: button on off led

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express()
+const httpServer = http.createServer(app)
+const io = new Server(httpServer, { cors: { origin: '*' } })
+
+app.use(express.static('views'))
+
+let buttonState = false;
+
+io.on('connection', socket => {
+    console.log('New Connection');
+
+    io.to(socket.id).emit('buttonState', buttonState);
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected');
+    })
+
+    socket.on('buttonState', value => {
+        console.log('buttonState: ', buttonState);
+        buttonState = value;
+        socket.broadcast.emit('buttonState', value);
+    })
+})
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+httpServer.listen(process.env.PORT || 4001, () => {
+    console.log('Running on PORT: ', httpServer.address());
+});
